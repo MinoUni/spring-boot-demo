44046d4e4201407ef7c37f7e26620a51ba878da9
MinoUni <maks.reva.ua@gmail.com>
2023-03-15 22:04:20 +0200

    remove redundant db_schema sql script & main class name refactor

--- src/main/java/com/miniuni/Application.java
+++ src/main/java/com/miniuni/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootTestAppApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootTestAppApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
