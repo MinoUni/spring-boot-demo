bd52edaecfe8acd0ac1b3f279812029468e47bec
MinoUni <maks.reva.ua@gmail.com>
2023-03-16 17:46:55 +0200

    add constant classes

---
+++ src/main/java/com/miniuni/constants/ErrorMessage.java
@@ -0,0 +1,6 @@
+package com.miniuni.constants;
+
+public final class ErrorMessage {
+
+    public static final  String USER_NOT_FOUND_BY_ID = "User with this id not exist: ";
+}
---
+++ src/main/java/com/miniuni/constants/LogMessage.java
@@ -0,0 +1,6 @@
+package com.miniuni.constants;
+
+public final class LogMessage {
+
+    public static final String IN_FIND_BY_ID = "In getById() method, id: {}";
+}
