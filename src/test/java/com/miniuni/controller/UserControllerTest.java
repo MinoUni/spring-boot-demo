505baa1d457de4cf21be0dd8ddf0a829126a4b6e
MinoUni <maks.reva.ua@gmail.com>
2023-03-16 22:24:18 +0200

    add UserController tests

--- src/test/java/com/miniuni/controller/UserControllerTest.java
+++ src/test/java/com/miniuni/controller/UserControllerTest.java
@@ -1,41 +1,58 @@
 package com.miniuni.controller;
 
+import com.miniuni.dto.UserDto;
+import com.miniuni.exception.UserNotFoundException;
+import com.miniuni.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.BDDMockito.given;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@ActiveProfiles("test")
+
+@WebMvcTest
+@ExtendWith(MockitoExtension.class)
 class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private UserServiceImpl userService;
+
     @Test
-    void getUserShouldReturnUserDtoWithResponse200() throws Exception {
-        this.mockMvc.perform(get("/api/v1/users/1"))
+    public void getUserShouldReturnUserDtoWithResponse200() throws Exception {
+        Long id = 1L;
+        UserDto user = new UserDto("Test-first", "Test-last", 10);
+        given(userService.getUserById(id)).willReturn(user);
+        this.mockMvc.perform(get("/api/v1/users/{id}", id)
+                .contentType(APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("firstName").value("Amber"))
-                .andExpect(jsonPath("lastName").value("Stark"))
-                .andExpect(jsonPath("age").value("23"));
+                .andExpect(jsonPath("firstName").value("Test-first"))
+                .andExpect(jsonPath("lastName").value("Test-last"))
+                .andExpect(jsonPath("age").value("10"));
     }
 
     @Test
-    void getUserShouldReturnExceptionWithResponse404() throws Exception {
-        this.mockMvc.perform(get("/api/v1/users/{id}", 99))
+    public void getUserShouldReturnUserNotFoundExceptionWithResponse404() throws Exception {
+        Long id = 1L;
+        given(userService.getUserById(id))
+                .willThrow(new UserNotFoundException("User with this id not exist: " + id));
+        this.mockMvc.perform(get("/api/v1/users/{id}", id)
+                .contentType(APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isNotFound())
-                .andExpect(jsonPath("message").value("User with this id not exist: 99"))
+                .andExpect(jsonPath("message").value("User with this id not exist: " + id))
                 .andExpect(jsonPath("httpStatus").value("NOT_FOUND"));
     }
 }
