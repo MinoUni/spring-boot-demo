3a648f4e7cb54bbb9f2d2fc44e70ee93f4222727
MinoUni <maks.reva.ua@gmail.com>
2023-03-16 17:53:13 +0200

    Add e2e test for getUserById endpoint

---
+++ src/test/java/com/miniuni/controller/UserControllerIntTest.java
@@ -0,0 +1,43 @@
+package com.miniuni.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource("/application.yml")
+class UserControllerIntTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getUserWithResponse200() throws Exception {
+        this.mockMvc.perform(get("/api/v1/users/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("firstName").value("Amber"))
+                .andExpect(jsonPath("lastName").value("Stark"))
+                .andExpect(jsonPath("age").value("23"));
+    }
+
+    @Test
+    void getUserWithResponse404() throws Exception {
+        this.mockMvc.perform(get("/api/v1/users/{id}", 99))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("message").value("User with this id not exist: 99"))
+                .andExpect(jsonPath("httpStatus").value("NOT_FOUND"));
+    }
+}
